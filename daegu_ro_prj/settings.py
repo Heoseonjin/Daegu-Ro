46881f91c6441cf69d069c236ae09fbb7fbee53f
SeonJin Heo <Heoseonjin@github.com>
2022-09-25 11:47:11 +0900

    app create

--- daegu_ro_prj/settings.py
+++ daegu_ro_prj/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'single_pages',
+    'recommend',
+    'noticeboard',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
