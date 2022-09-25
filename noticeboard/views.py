ce89a7acbee189b9a384a87b6ebfc936e70c1b3c
SeonJin Heo <Heoseonjin@github.com>
2022-09-25 13:10:55 +0900

    board_list, board_detail create with CBV method

--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+
+# CBV 방식 사용
+# 목록
+class BoardList(ListView):
+    model = Post
+    template_name = 'noticeboard/board_list.html'
+    ordering = '-pk'
+
+
+# 상세페이지
+class BoardDetail(DetailView):
+    model = Post
+    template_name = 'noticeboard/board_detail.html'
