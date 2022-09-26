a158c1e53f6ec8afcea926ad7fb31cd53d6a6704
SeonJin Heo <Heoseonjin@github.com>
2022-09-26 11:56:58 +0900

    base, navbar, footer html create

--- daegu_ro_prj/urls.py
+++ daegu_ro_prj/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('noticeboard.urls')),
+    path('', include('single_pages.urls')),
+    path('board/', include('noticeboard.urls')),
     path('admin/', admin.site.urls),
 ]
